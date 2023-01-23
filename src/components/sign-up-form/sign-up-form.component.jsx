82bc33740eff948c2438e3a2a1e84b4b228dba3a
Kristian Beall <beallkristian@gmail.com>
2023-01-23 11:55:17 -0500

    created button component with inverted, google, and default styling

--- src/components/sign-up-form/sign-up-form.component.jsx
+++ src/components/sign-up-form/sign-up-form.component.jsx
@@ -1,8 +1,11 @@
 import { useState } from 'react';
 
 import FormInput from '../form-input/form-input.component';
+import Button from '../button/button.component';
 
 import { createAuthUserWithEmailAndPassword, createUserDocumentFromAuth } from '../../utils/firebase/firebase.utils';
+
+import './sign-up-form.styles.scss';
 
 const defaultFormFields = {
   displayName: '',
@@ -49,8 +52,9 @@ const handleChange = (event) => {
 };
 
   return (
-    <div>
-      <h1>Sign up with your email and password</h1>
+    <div className="sign-up-container">
+      <h2>Don't have an account?</h2>
+      <span>Sign up with your email and password</span>
       <form onSubmit={handleSubmit}>
         <FormInput label="Display Name" type="text" required onChange={handleChange} name="displayName" value={displayName} />
 
@@ -59,7 +63,7 @@ const handleChange = (event) => {
         <FormInput label="Password" type="password" required onChange={handleChange} name="password" value={password} />
 
         <FormInput label="Confirm Password" type="password" required onChange={handleChange} name="confirmPassword" value={confirmPassword} />
-        <button type="submit">Sign Up</button>
+        <Button type="submit">Sign Up</Button>
       </form>
     </div>
   );
